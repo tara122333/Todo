564485abdcd8a4bef8744c7f6758ccbf159e1ad4
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-09-22 11:07:21 +0530

    delete todo task api and get all task api

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -4,8 +4,6 @@ exports.addTodo = async (req, res) => {
     try {
         const { _id } = req.params;
         const { task } = req.body;
-        console.log("task");
-        console.log(task);
         const user = await UserModel.findOne({ _id });
         if (!user) {
             return res.status(203).json({ message: "not found" });
@@ -33,6 +31,50 @@ exports.addTodo = async (req, res) => {
             const taskData = await TaskModel.create(createTask);
             return res.status(200).json({ message: "task added success", taskData });
         }
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getTodo = async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const user = await UserModel.findOne({ _id });
+        if (!user) {
+            return res.status(203).json({ message: "not found" });
+        }
+        const getUserTask = await TaskModel.findOne({ user: _id });
+        if (getUserTask) {
+            return res.status(200).json({ message: "all task", getUserTask });
+        }
+        return res.status(201).json({ message: "task not found"});
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+exports.deleteTodoTask = async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const findUserTask = await TaskModel.findOne({ 'task._id': _id });
+        await TaskModel.updateOne(
+            {
+                _id : findUserTask._id
+            },
+            {
+                $pull : {
+                    task : {
+                        _id : _id
+                    }
+                }
+            },
+            {
+                new : true
+            }
+        )
+        return res.status(200).json({ message: "task delete success" });
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
--- server/router/todoRouter.js
+++ server/router/todoRouter.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const todoController = require('../controller/todoController');
 
 router.post('/add/:_id', todoController.addTodo);
+router.get('/get/:_id', todoController.getTodo);
+router.delete('/delete/:_id', todoController.deleteTodoTask);
 
 export default router;
 
